4579982bcc24eb0d345af342355e8fc9f0d74bea
AlexEvdokimov10 <56792898+AlexEvdokimov10@users.noreply.github.com>
2021-09-09 12:55:16 +0300

    first exercise

---
+++ FirstProj/resources/views/myTemplateBlade.php
@@ -0,0 +1,19 @@
+
+<?php 
+$value="Evdokimov 209 "
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My first proj</title>
+</head>
+<body>
+	<h1>
+		Hello world !!
+		<?php echo $value ?>
+	</h1>
+</body>
+</html>
